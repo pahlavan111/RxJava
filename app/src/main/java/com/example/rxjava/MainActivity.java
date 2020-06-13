2dce008ebf691f1435856c3c1cebab48afb7338b
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-13 20:24:14 +0430

    modified mainActivitu to add 2 button an use asyncTask to get data

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
+import io.reactivex.disposables.CompositeDisposable;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,32 +19,48 @@ public class MainActivity extends AppCompatActivity {
     private static final String link = "https://finepointmobile.com/data/products.json";
     public static String content ="";
 
+    Button btn_asynk,btn_rxjava;
+
+    private CompositeDisposable compositeDisposable=new CompositeDisposable();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // final CompositeDisposable compositeDisposable=new CompositeDisposable();
+        btn_asynk=findViewById(R.id.btn_Async);
+        btn_rxjava = findViewById(R.id.btn_rxjava);
 
-        final AsyncTask execute = new AsyncTaskGetData(link).execute();
-
-        final Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
+        btn_asynk.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
+            public void onClick(View v) {
+
+                final AsyncTask execute = new AsyncTaskGetData(link).execute();
+
+                final Timer timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
                     @Override
                     public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
 
-                        if (!content.equals("")){
-                            Toast.makeText(MainActivity.this, content+"", Toast.LENGTH_SHORT).show();
-                            timer.cancel();
-                           // finish();
-                        }
+                                if (!content.equals("")){
+                                    Toast.makeText(MainActivity.this, content+"", Toast.LENGTH_SHORT).show();
+                                    timer.cancel();
+                                    // finish();
+                                }
+                            }
+                        });
                     }
-                });
+                },10,50);
             }
-        },10,50);
+        });
+
+
+
+
+
 
     }
 
