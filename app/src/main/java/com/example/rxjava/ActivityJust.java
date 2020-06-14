8fd9c5dc72adc48796df451b932f789f46f63d02
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-14 13:41:52 +0430

    adding Disoisable to activity Just

--- app/src/main/java/com/example/rxjava/ActivityJust.java
+++ app/src/main/java/com/example/rxjava/ActivityJust.java
@@ -9,24 +9,32 @@ import android.widget.TextView;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class ActivityJust extends AppCompatActivity {
-    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    private Disposable disposable;
     private String TAG = "Behrooz";
     TextView textView;
-    String res="";
+    String res = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_just);
-        textView= findViewById(R.id.textView_just);
+        textView = findViewById(R.id.textView_just);
 
+        // observable
         Observable<String> animalObservable = getAnimalObservable();
-        animalObservable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(getAnimalObserver());
+
+        // observer
+        Observer<String> animalsObserver = getAnimalObserver();
+
+        // observer subscribing to observable
+        animalObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);
 
     }
 
@@ -40,21 +48,21 @@ public class ActivityJust extends AppCompatActivity {
             @Override
             public void onSubscribe(Disposable d) {
                 Log.d(TAG, "onSubscribe");
-                res=res+"onSubscribe\n";
-                compositeDisposable.add(d);
+                res = res + "onSubscribe\n";
+                disposable = d;
             }
 
             @Override
             public void onNext(String s) {
                 Log.d(TAG, s);
-                res=res+s+"\n";
+                res = res + s + "\n";
             }
 
             @Override
             public void onError(Throwable e) {
 
                 Log.d(TAG, "OnError: " + e.toString());
-                res=res+"OnError: " + e.toString()+"\n";
+                res = res + "OnError: " + e.toString() + "\n";
 
             }
 
@@ -62,14 +70,20 @@ public class ActivityJust extends AppCompatActivity {
             public void onComplete() {
 
                 Log.d(TAG, "All item are emmited");
-                res=res+"All item are emmited";
+                res = res + "All item are emmited";
                 textView.setText(res);
-                res="";
+                res = "";
 
-                if (compositeDisposable.size() != 0) {
-                    compositeDisposable.clear();
-                }
             }
+
         };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // don't send events once the activity is destroyed
+        disposable.dispose();
     }
 }
