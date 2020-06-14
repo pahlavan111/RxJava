af7baf24a828f94531478782a414eb2da616b6b5
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-14 10:42:12 +0430

    getting ready to use rxjava and modified a button to start just in rx java

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,19 +23,19 @@ import io.reactivex.disposables.CompositeDisposable;
 public class MainActivity extends AppCompatActivity {
 
     private static final String link = "https://finepointmobile.com/data/products.json";
-    public static String content ="";
+    public static String content = "";
 
-    Button btn_asynk,btn_rxjava;
+    Button btn_asynk, btn_just;
 
-    private CompositeDisposable compositeDisposable=new CompositeDisposable();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_asynk=findViewById(R.id.btn_Async);
-        btn_rxjava = findViewById(R.id.btn_rxjava);
+        btn_asynk = findViewById(R.id.btn_Async);
+        btn_just = findViewById(R.id.btn_just);
 
         btn_asynk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,27 +51,61 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
 
-                                if (!content.equals("")){
-                                    Toast.makeText(MainActivity.this, content+"", Toast.LENGTH_SHORT).show();
+                                if (!content.equals("")) {
+                                    Toast.makeText(MainActivity.this, content + "", Toast.LENGTH_SHORT).show();
                                     timer.cancel();
                                     // finish();
                                 }
                             }
                         });
                     }
-                },10,50);
+                }, 10, 50);
             }
         });
 
 
+        btn_just.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+
+            }
+        });
 
 
     }
 
 
 
+    String getData(String link) {
+
+        content = "";
+
+        try {
+            URL url = new URL(link);
+            URLConnection connection = url.openConnection();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            content = builder.toString();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return content;
+    }
 
 
 }
